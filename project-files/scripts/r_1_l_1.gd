feef88f41e38cf378944af0b2bcdc7a29d43c741
= <170835528+FinalFenetix@users.noreply.github.com>
2025-02-24 07:24:43 +1000

    feat: Added animation for reality shifting ability

--- project-files/scripts/r_1_l_1.gd
+++ project-files/scripts/r_1_l_1.gd
@@ -1,10 +1,19 @@
 extends Node2D
-
+@onready var animation: AnimationPlayer = $"../R2_L1/AnimationPlayer"
+var reality = 0
 func _process(delta: float) -> void:
+	if Global.CurrentReality != reality:
+		change_reality()
+
+func change_reality():
 	if Global.CurrentReality == 1:
-		show()
+		reality = 1
+		animation.play("fadeIn")
 		position = Vector2(0, 0)
-		
+
 	else:
-		hide()
+		reality = 2
+		animation.play("fadeOut")
+		await animation.animation_finished
 		position = Vector2(99999, 99999)
+		
--- project-files/scripts/r_2_l_1.gd
+++ project-files/scripts/r_2_l_1.gd
@@ -1,10 +1,19 @@
 extends Node2D
+@onready var animation: AnimationPlayer = $AnimationPlayer
 
+var reality = 0
 func _process(delta: float) -> void:
+	if Global.CurrentReality != reality:
+		change_reality()
+
+func change_reality():
 	if Global.CurrentReality == 2:
-		show()
+		reality = 2
+		animation.play("fadeIn")
 		position = Vector2(0, 0)
-		
+
 	else:
-		hide()
+		reality = 1
+		animation.play("fadeOut")
+		await animation.animation_finished
 		position = Vector2(99999, 99999)
