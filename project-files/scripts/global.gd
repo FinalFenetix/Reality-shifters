4ba9359502e9d4fdbdb3e51070d9a7d264acefaa
= <170835528+FinalFenetix@users.noreply.github.com>
2025-02-23 18:45:12 +1000

    feat: finished reality system, no vfx added for transition

--- project-files/scripts/global.gd
+++ project-files/scripts/global.gd
@@ -1,1 +1,11 @@
 extends Node
+
+var CurrentReality = 1
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("switch_reality"):
+		if CurrentReality == 1:
+			CurrentReality = 2
+
+		else:
+			CurrentReality = 1
