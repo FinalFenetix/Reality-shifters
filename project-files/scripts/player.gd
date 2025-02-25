b3b4db4cc58f542c765a5244573a652737435448
= <170835528+FinalFenetix@users.noreply.github.com>
2025-02-25 20:43:14 +1000

    feat: finished prototype, ready for development

--- project-files/scripts/player.gd
+++ project-files/scripts/player.gd
@@ -5,15 +5,23 @@ var canRegen = 0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var AbilityCount = 3
-@onready var timer: Timer = $Timer
+@onready var timer: Timer = $JumpCooldown
 @onready var JumpUI: AnimatedSprite2D = $jumpUI
+@onready var StaminaBar: TextureProgressBar = $TextureProgressBar
+@onready var progress_bar_cooldown: Timer = $progressBarCooldown
+@onready var increase_progress_bar: Timer = $increaseProgressBar
+var StaminaCanRegen = 0
+var StaminaTimerWait = 1
 
+func _ready() -> void:
+	StaminaBar.value = 100
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		canRegen = 0
+		StaminaCanRegen = 0
 	
 	else:
 		if AbilityCount == 0:
@@ -22,6 +30,10 @@ func _physics_process(delta: float) -> void:
 		if timerWait == 1 and AbilityCount < 3:
 			timerStart()
 		canRegen = 1
+		
+		if StaminaTimerWait == 1 and StaminaBar.value < 100:
+			StaminaTimerStart()
+		StaminaCanRegen = 1
 
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and AbilityCount > 0:
@@ -36,6 +48,10 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if direction and Input.is_action_just_pressed("dash") and StaminaBar.value > 33:
+		StaminaBar.value -= 30
+		velocity.x = direction * SPEED * 20
+	
 	move_and_slide()
 	
 	if AbilityCount == 3:
@@ -57,7 +73,26 @@ func timerStart():
 		await timer.timeout
 	timerWait = 1
 
+func StaminaTimerStart():
+	StaminaTimerWait = 0
+	if is_on_floor():
+		progress_bar_cooldown.start()
+		await timer.timeout
+	StaminaTimerWait = 1
 
 func _on_timer_timeout() -> void:
 	if canRegen == 1:
 		AbilityCount += 1
+
+
+func _on_progress_bar_cooldown_timeout() -> void: #ProgressBarCooldown
+	if StaminaCanRegen == 1:
+		while not StaminaBar.value > 99:
+			increase_progress_bar.start()
+			await increase_progress_bar.timeout
+
+
+
+
+func _on_increase_progress_bar_timeout() -> void:
+	StaminaBar.value += 5
