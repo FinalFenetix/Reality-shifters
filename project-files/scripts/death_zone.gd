677e919f163ea62f103972ee98e1b97adbf5f40e
Coffee and Errors <xuperwolfxb@gmail.com>
2025-02-26 18:09:41 +1000

    Prototype (#4)
    * feat: Added changing realities mechanic, not very good, work in progress
    
    * feat: finished reality system, no vfx added for transition
    
    * feat: Added animation for reality shifting ability
    
    * feat: Added camera limit, added way to die after falling too much, worked on map for a bit
    
    * feat: Added extra jump mechanics, almost done just need to add dashing
    
    * feat: finished prototype, ready for development
    
    * fix: Reworked the dash ability, now costs 2 ability points

---
+++ project-files/scripts/death_zone.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var BodyList = ""
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5
+	timer.start()
+	body.get_node("CollisionShape2D").queue_free()
+	
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
+	Global.CurrentReality = 1
---
+++ project-files/scripts/r_1_l_1.gd
@@ -0,0 +1,19 @@
+extends Node2D
+@onready var animation: AnimationPlayer = $"../R1_L1/AnimationPlayer"
+var reality = 0
+func _process(delta: float) -> void:
+	if Global.CurrentReality != reality:
+		change_reality()
+
+func change_reality():
+	if Global.CurrentReality == 1:
+		reality = 1
+		animation.play("fadeIn")
+		position = Vector2(0, 0)
+
+	else:
+		reality = 2
+		animation.play("fadeOut")
+		await animation.animation_finished
+		position = Vector2(99999, 99999)
+		
